73fd0114aead9290d06eebab3c1919b122cffe7a
JJablon <jackeneczek@gmail.com>
2017-10-31 21:55:45 +0100

    initial

--- AutoVPNDisconnect/Form1.Designer.cs
+++ AutoVPNDisconnect/Form1.Designer.cs
@@ -29,11 +29,148 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(356, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose app to stop when no VPN connection";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(356, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 178);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(409, 25);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(50, 20);
+            this.toolStripStatusLabel1.Text = "status:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(13, 93);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(163, 84);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Found IP addresses";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(202, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "VPN IP address start with:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(421, 83);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(236, 117);
+            this.textBox3.TabIndex = 7;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(409, 203);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "VPN Failsafe Killswitch by JJablon@github";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
     }
 }
 
--- AutoVPNDisconnect/Form1.cs
+++ AutoVPNDisconnect/Form1.cs
@@ -3,18 +3,171 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Resources;
+using System.Reflection;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
 
 namespace AutoVPNDisconnect
 {
     public partial class Form1 : Form
     {
+        //IResourceWriter writer = new ResourceWriter("Resource1.resx");
+
         public Form1()
         {
             InitializeComponent();
+            this.notifyIcon1.Icon = SystemIcons.Shield;
+            this.notifyIcon1.ShowBalloonTip(10000, "VPN Failsafe Killswitch", "VPN Failsafe Killswitch running in the background..", new ToolTipIcon());
+            toolStripStatusLabel1.Text = "Status: ";
+            this.timer1.Interval = 1;
+            this.Visible = false;
+            this.FormClosing += Form1_FormClosing;
+            string path =
+            AutoVPNDisconnect.Resource1.ResourceManager.GetString("path");
+            
+            string IP = AutoVPNDisconnect.Resource1.ResourceManager.GetString("ip");
+            
+            if (path == null || path == "") { }
+            else
+            {
+                this.textBox1.Text = path;
+            }
+            if (IP == null || IP == "") { }
+            else
+            {
+                this.textBox2.Text = IP;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+       
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.DefaultExt = "exe";
+            opf.CheckFileExists = true;
+            opf.CheckPathExists = true;
+            opf.Multiselect = false;
+            opf.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+            
+            if(opf.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = opf.FileName;
+
+
+            }
+        }
+        public  List<string> GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            List<String> list = new List<string>();
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    list.Add(ip.ToString());
+                }
+            }
+            return list;
+            //throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.timer1.Interval = 2000;
+            this.listBox1.Items.Clear();
+            List<string> list = this.GetLocalIPAddress();
+            this.listBox1.Items.AddRange(list.ToArray());
+
+            /*
+            Process p = new Process();
+            // Redirect the output stream of the child process.
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.Arguments = "tasklist";
+            p.StartInfo.CreateNoWindow = false;
+
+            p.Start();
+            // Do not wait for the child process to exit before
+            // reading to the end of its redirected stream.
+            // p.WaitForExit();
+            // Read the output stream first and then wait.
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            this.textBox3.Text = output;
+
+    */
+            bool flag = false;
+            bool flag_found = false;
+            var allProcceses = Process.GetProcesses();
+            foreach (Process p in allProcceses)
+            {
+                if (p.ProcessName == textBox1.Text.Substring(textBox1.Text.LastIndexOf("\\") + 1, textBox1.Text.Length + 2 - textBox1.Text.LastIndexOf(".")))
+                    flag_found = true;
+            }
+
+            foreach (var item in list)
+            {
+                if (item.IndexOf(textBox2.Text) == 0)
+                {
+                    flag = true;
+                }
+            }
+            if (flag == true) { toolStripStatusLabel1.Text = "Status: VPN CONNECTED"; notifyIcon1.Text = "Status: VPN CONNECTED"; }
+            else {toolStripStatusLabel1.Text = "Status: VPN DISCONNECTED"; notifyIcon1.Text = "Status: VPN DISCONNECTED"; }
+
+            if (flag == false&&flag_found==true)
+            {
+                System.Diagnostics.Process.Start("cmd.exe", "/c taskkill /F /IM " + textBox1.Text.Substring(textBox1.Text.LastIndexOf("\\") + 1));
+
+                //Process myprc = GetaProcess(textBox1.Text.Substring(textBox1.Text.LastIndexOf("\\") + 1,textBox1.Text.Length+2- textBox1.Text.LastIndexOf(".")));
+                // try
+                // {
+                //   myprc.Kill();
+                //}
+                // catch (NullReferenceException) { }
+            }
+            else
+            {
+              
+
+            }
+        }
+
+        private Process GetaProcess(string processname)
+        {
+            Process[] aProc = Process.GetProcesses();//Process.GetProcessesByName(processname);
+            bool flag_found = false;
+            foreach (Process p in aProc)
+            {
+                if (p.ProcessName == textBox1.Text.Substring(textBox1.Text.LastIndexOf("\\") + 1))
+                    flag_found = true;
+                     return p;
+            }
+            if (aProc.Length > 0)
+                return aProc[0];
+
+            else return null;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = !this.Visible;
         }
     }
 }
