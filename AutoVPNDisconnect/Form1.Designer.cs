371a96e3d80b52c2214637facbac3a7b83c48d14
Jacek Jabłoński <jackeneczek@gmail.com>
2017-11-09 11:26:50 +0100

    autoreconnect, autostart app

--- AutoVPNDisconnect/Form1.Designer.cs
+++ AutoVPNDisconnect/Form1.Designer.cs
@@ -40,24 +40,33 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Connect = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(26, 40);
+            this.button1.Location = new System.Drawing.Point(20, 32);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(356, 23);
+            this.button1.Size = new System.Drawing.Size(267, 24);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Choose app to stop when no VPN connection";
+            this.button1.Text = "Choose an app to stop when no VPN connection";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(26, 12);
+            this.textBox1.Location = new System.Drawing.Point(20, 10);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(356, 22);
+            this.textBox1.Size = new System.Drawing.Size(268, 20);
             this.textBox1.TabIndex = 1;
             // 
             // timer1
@@ -71,58 +80,63 @@
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 178);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 143);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(409, 25);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(307, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(50, 20);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(41, 17);
             this.toolStripStatusLabel1.Text = "status:";
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(13, 93);
+            this.listBox1.Location = new System.Drawing.Point(10, 76);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(163, 84);
+            this.listBox1.Size = new System.Drawing.Size(123, 69);
             this.listBox1.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 71);
+            this.label1.Location = new System.Drawing.Point(8, 58);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(134, 17);
+            this.label1.Size = new System.Drawing.Size(101, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "Found IP addresses";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(202, 92);
+            this.textBox2.Location = new System.Drawing.Point(152, 72);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
             this.textBox2.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(199, 70);
+            this.label2.Location = new System.Drawing.Point(149, 57);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(171, 17);
+            this.label2.Size = new System.Drawing.Size(130, 13);
             this.label2.TabIndex = 6;
             this.label2.Text = "VPN IP address start with:";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(421, 83);
+            this.textBox3.Location = new System.Drawing.Point(316, 67);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(236, 117);
+            this.textBox3.Size = new System.Drawing.Size(178, 96);
             this.textBox3.TabIndex = 7;
             // 
             // notifyIcon1
@@ -132,11 +146,72 @@
             this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Connect,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(120, 48);
+            // 
+            // Connect
+            // 
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(152, 22);
+            this.Connect.Text = "Connect";
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(152, 94);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Autokill";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(152, 111);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(89, 17);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "Autostart app";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(152, 128);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(99, 17);
+            this.checkBox3.TabIndex = 10;
+            this.checkBox3.Text = "Auto reconnect";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(409, 203);
+            this.ClientSize = new System.Drawing.Size(307, 165);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
@@ -146,6 +221,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -153,6 +229,7 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,6 +248,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Connect;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- AutoVPNDisconnect/Form1.cs
+++ AutoVPNDisconnect/Form1.cs
@@ -12,6 +12,8 @@ using System.Reflection;
 using System.Net;
 using System.Net.Sockets;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+
 
 namespace AutoVPNDisconnect
 {
@@ -142,16 +144,19 @@ namespace AutoVPNDisconnect
               
 
             }
+            if (flag_last != null && flag_last == false && flag == true && checkBox2.Checked) System.Diagnostics.Process.Start(textBox1.Text);
+            if (flag_last != null && flag_last == true && flag == false && checkBox3.Checked) Connect_VPN();
+            flag_last = flag;
         }
-
+        bool? flag_last = null;
         private Process GetaProcess(string processname)
         {
             Process[] aProc = Process.GetProcesses();//Process.GetProcessesByName(processname);
-            bool flag_found = false;
+            //bool flag_found = false;
             foreach (Process p in aProc)
             {
                 if (p.ProcessName == textBox1.Text.Substring(textBox1.Text.LastIndexOf("\\") + 1))
-                    flag_found = true;
+                    //flag_found = true;
                      return p;
             }
             if (aProc.Length > 0)
@@ -167,7 +172,83 @@ namespace AutoVPNDisconnect
 
         private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
         {
+            if(e.Button == MouseButtons.Left)
+                
             this.Visible = !this.Visible;
+            if (e.Button == MouseButtons.Right) {
+                var r = WinAPI.GetTrayRectangle();
+                contextMenuStrip1.Show(new Point(r.Right, r.Top));
+                    }
+                   
+        }
+        private void Connect_VPN()
+        {
+            try { 
+            var c = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\VPN.txt";
+            var contents = File.ReadAllText(c);
+        
+
+
+            System.Diagnostics.Process.Start("rasdial.exe", contents);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Please create a file VPN.txt in %appdata% with contents like:VPNConnectionName VPNUsername VPNPassword");
+            }
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            Connect_VPN();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+    public class WinAPI
+    {
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+
+            public override string ToString()
+            {
+                return "(" + left + ", " + top + ") --> (" + right + ", " + bottom + ")";
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr FindWindow(string strClassName, string strWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, IntPtr windowTitle);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+
+        public static IntPtr GetTrayHandle()
+        {
+            IntPtr taskBarHandle = WinAPI.FindWindow("Shell_TrayWnd", null);
+            if (!taskBarHandle.Equals(IntPtr.Zero))
+            {
+                return WinAPI.FindWindowEx(taskBarHandle, IntPtr.Zero, "TrayNotifyWnd", IntPtr.Zero);
+            }
+            return IntPtr.Zero;
+        }
+
+        public static Rectangle GetTrayRectangle()
+        {
+            WinAPI.RECT rect;
+            WinAPI.GetWindowRect(WinAPI.GetTrayHandle(), out rect);
+            return new Rectangle(new Point(rect.left, rect.top), new Size((rect.right - rect.left) + 1, (rect.bottom - rect.top) + 1));
         }
     }
 }
